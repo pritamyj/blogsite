48b117a92d5450cf5b63900de29179ea960dc448
pritamjadhav <88623137+pritamyj@users.noreply.github.com>
2021-08-31 22:31:33 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-
-// if the user clicks on the like button ...
+ 
 $('.like-btn').on('click', function(){
   var post_id = $(this).data('id');
   $clicked_btn = $(this);
@@ -24,19 +23,16 @@ $('.like-btn').on('click', function(){
   		} else if(action == "unlike") {
   			$clicked_btn.removeClass('fa-thumbs-up');
   			$clicked_btn.addClass('fa-thumbs-o-up');
-  		}
-  		// display the number of likes and dislikes
+  		} 
   		$clicked_btn.siblings('span.likes').text(res.likes);
   		$clicked_btn.siblings('span.dislikes').text(res.dislikes);
-
-  		// change button styling of the other button if user is reacting the second time to post
+ 
   		$clicked_btn.siblings('i.fa-thumbs-down').removeClass('fa-thumbs-down').addClass('fa-thumbs-o-down');
   	}
   });		
 
 });
-
-// if the user clicks on the dislike button ...
+ 
 $('.dislike-btn').on('click', function(){
   var post_id = $(this).data('id');
   $clicked_btn = $(this);
@@ -60,12 +56,10 @@ $('.dislike-btn').on('click', function(){
   		} else if(action == "undislike") {
   			$clicked_btn.removeClass('fa-thumbs-down');
   			$clicked_btn.addClass('fa-thumbs-o-down');
-  		}
-  		// display the number of likes and dislikes
+  		} 
   		$clicked_btn.siblings('span.likes').text(res.likes);
   		$clicked_btn.siblings('span.dislikes').text(res.dislikes);
-  		
-  		// change button styling of the other button if user is reacting the second time to post
+  		 
   		$clicked_btn.siblings('i.fa-thumbs-up').removeClass('fa-thumbs-up').addClass('fa-thumbs-o-up');
   	}
   });	
